12715f62a65f90ce3f69d1aa863e1f186fa34352
Pavel Gurevich <pavelgurevichwork@gmail.com>
2024-05-03 17:09:27 +0300

    finished openapi

--- account-service/src/main/java/com/grewu/account/controller/open_api/AccountOpenApi.java
+++ account-service/src/main/java/com/grewu/account/controller/open_api/AccountOpenApi.java
@@ -16,7 +16,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
 
-@Tag(name = "User")
+@Tag(name = "Account")
 public interface AccountOpenApi {
     @Operation(
             method = "GET",
